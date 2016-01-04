6ecc943d24c5c1b3c02bab41e2800ea17bbf41da
nir kaufman <nirkaufman@gmail.com>
2016-01-05 01:06:57 +0200

    coc state

--- src/states/states.module.js
+++ src/states/states.module.js
@@ -1,4 +1,5 @@
 import {HomeController} from './home/home';
+import {CocController}  from './coc/coc';
 
 // define application module
 function routes($stateProvider, $urlRouterProvider) {
@@ -9,9 +10,14 @@ function routes($stateProvider, $urlRouterProvider) {
       url: '/home',
       controller: 'HomeController',
       templateUrl: 'states/home/home.html'
+    })
+    .state('coc', {
+      url: '/coc',
+      controller: 'CocController',
+      templateUrl: 'states/coc/coc.html'
     });
 }
 
 export default angular.module('angularCamp.states', [])
   .config(routes)
-  .controller({HomeController});
+  .controller({HomeController, CocController});
